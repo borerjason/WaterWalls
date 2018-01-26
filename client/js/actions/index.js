a5aaf8c4027d0af85bd6719579499f405eecd416
Jason Borer <borerjason@gmail.com>
2018-01-26 15:25:48 -0800

    add fetch blocks helper function to front end

---
+++ client/js/actions/index.js
@@ -0,0 +1,13 @@
+
+const fetchBlocks = walls => (
+  fetch('/waterwalls', {
+    method: "POST",
+    body: JSON.stringify( { walls }),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  })
+    .then(response => response.json()) 
+)
+
+module.exports = fetchBlocks;
--- client/js/index.js
+++ client/js/index.js
@@ -1,1 +1,9 @@
-console.log('hello');
+const formButton = document.getElementById('search');
+const formInput = document.getElementById('entry');
+
+formButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const input = formInput.value;
+  formInput.value = "";
+
+});
